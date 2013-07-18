5c0911c601572085b4a8c97de3736565f4321ab6
Wai <waichak.luk@gmail.com>
2013-07-18 04:57:10 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 app.get('/', function(request, response) {
 var fs = require('fs');
@@ -10,7 +10,7 @@ var data = fs.readFileSync('index.html');
 
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
